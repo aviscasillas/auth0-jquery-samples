c185d54edf0ca8a85c4d05172a222688b70f4e15
Albert Viscasillas <aviscasillas@gmail.com>
2016-08-29 09:35:28 +0200

    POC

--- 01-Login/auth0-variables.js
+++ 01-Login/auth0-variables.js
@@ -1,3 +1,3 @@
-var AUTH0_CLIENT_ID='{CLIENT_ID}';
-var AUTH0_DOMAIN='{DOMAIN}';
+var AUTH0_CLIENT_ID='JithnTeyBORBA1Dg0jboOcpx4VHTHt2m';
+var AUTH0_DOMAIN='aviscasillas.eu.auth0.com';
 var AUTH0_CALLBACK_URL=location.href;
